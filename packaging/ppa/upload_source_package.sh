bd7db794ac86b5aeb828dd062007952a58079a3d
Marin <p1-mmr@users.noreply.github.com>
2026-03-19 05:18:09 +0100

    Try to fix Launchpad upload failing with "has 57 file(s) with a time stamp too far in the past"

--- packaging/ppa/upload_source_package.sh
+++ packaging/ppa/upload_source_package.sh
@@ -7,8 +7,22 @@ set -xe
 cd "$(dirname "$0")"
 
 PKGNAME="qcsuper"
-PKGVER="2.1.0.post3"
+PKGVER="2.1.0.post4"
 PKGREL="1"
+
+PPA_DIR="$(pwd)"
+DEB_DIST_DIR="${PPA_DIR}/deb_dist"
+
+rm -rf "${DEB_DIST_DIR}"
+
+# Create a target directory for our new source package before we build it
+temp_dir="$(mktemp -d)"
+
+function cleanup_dirs {
+    rm -rf "${temp_dir}"
+}
+
+trap cleanup_dirs INT TERM
 
 # Needed to generate a .tar.gz that will contain a setup.py
 # file generated from our pyproject.toml located at
@@ -16,11 +30,27 @@ PKGREL="1"
 
 poetry build -f sdist
 
+DIST_FILE="${PPA_DIR}/../../dist/${PKGNAME}-${PKGVER}.tar.gz"
+
+cd "${temp_dir}"
+
+tar xvf "${DIST_FILE}"
+
+find . -exec touch {} \;
+
+rm -f "${DIST_FILE}"
+
+tar zcvf "${DIST_FILE}" .
+
+cd "${PPA_DIR}"
+
 for version in noble questing resolute; do # bionic focal jammy noble questing
 
     py2dsc-deb -x stdeb.cfg --suite ${version} --sign-results --sign-key 87EC6DB535CC2A084B41E88EF675C22E1B4B2ACC \
-        --debian-version ${PKGREL}${version} ../../dist/${PKGNAME}-${PKGVER}.tar.gz
+        --debian-version ${PKGREL}${version} "${DIST_FILE}"
 
     dput ppa:marin-m/qcsuper "deb_dist/${PKGNAME}_${PKGVER}-${PKGREL}${version}_source.changes"
 
 done
+
+cleanup_dirs
